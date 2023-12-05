2d2fdea3b8d5c1a061da5eab69411dc916890fe3
Jeremy Chambon <jeremy.chambon2906@gmail.com>
2023-12-05 16:30:52 +0100

    COMMIT

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let btn = document.getElementById("btn");
+let dollars = document.getElementById("dollars");
+let little_grandma = document.getElementById("little_grandma");
+let little_cookie = document.getElementById("little_cookie");
+let cookie = document.getElementById("cookie");
+let nb = document.getElementById("cookie");
+let nb_grandma = document.getElementById("cookie");
+
+
+cookie.onclick = function () {
+    console.log("Coucou")
+    let c = nb.value;
+    c = c++
+}
+
+btn.onclick = function () {
+    console.log("Ajout 2 sur Grand mère")
+}
